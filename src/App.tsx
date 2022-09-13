98f66f84cd490d60c6542b6f3918d2e3fd0b5305
Pedro Barros <pedrobars7a@gmail.com>
2022-09-13 09:10:07 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/main.css'
+
+import logoImg from './assets/logo-nlw-esports.svg'
+
+function App() {
+ return (
+  <div className='w-8 h-8 bg-black text-zinc-400'>
+    <img src={logoImg} alt=''/>
+  </div>
+ );
+}
+
+export default App
