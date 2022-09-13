99fc0a9be475f678035f029c79195fe721d78f94
Pedro Barros <pedrobars7a@gmail.com>
2022-09-13 09:29:29 -0300

    instalando eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import './styles/main.css'
+import "./styles/main.css";
 
-import logoImg from './assets/logo-nlw-esports.svg'
+import logoImg from "./assets/logo-nlw-esports.svg";
 
 function App() {
- return (
-  <div className='w-8 h-8 bg-black text-zinc-400'>
-    <img src={logoImg} alt=''/>
-  </div>
- );
+  return (
+    <div className="w-8 h-8 bg-black text-zinc-400">
+      <img src={logoImg} alt="" />
+    </div>
+  );
 }
 
-export default App
+export default App;
